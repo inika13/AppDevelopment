ffe6ec00912630627fe3f9d2dc883f6aa4256f01
INIKA V <110533381+inika13@users.noreply.github.com>
2023-09-25 10:50:51 +0530

    Add files via upload

---
+++ Day10/src/pages/Class.jsx
@@ -0,0 +1,79 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+
+import {Container} from 'react-bootstrap'
+
+function Class() {
+    
+    return (
+        <>
+
+            <Layout />
+            <div className='main-wrapper'>
+            <div className='vidtxt'>
+            
+              
+                    <div className="ratio ratio-16x9">
+                          <iframe src="https://www.youtube.com/embed/VaoV1PrYft4" title="YouTube video" allowfullscreen></iframe>
+                    </div>
+                    
+                    <div className="ratio ratio-16x9">
+                          <iframe src="https://www.youtube.com/embed/dAqQqmaI9vY" title="YouTube video" allowfullscreen></iframe>
+                    </div>
+
+                    <div className="ratio ratio-16x9">
+                    <iframe src="https://www.youtube.com/embed/149Iac5fmoE" title="YouTube video" allowfullscreen></iframe>
+                    </div>
+              
+                    <div className="ratio ratio-16x9">
+                    <iframe src="https://www.youtube.com/embed/dAqQqmaI9vY" title="YouTube video" allowfullscreen></iframe>
+                   </div>
+
+                   <div className="ratio ratio-16x9">
+                    <iframe src="https://www.youtube.com/embed/yqeirBfn2j4" title="YouTube video" allowfullscreen></iframe>
+                   </div>
+
+                   <div className="ratio ratio-16x9">
+                    <iframe src="https://www.youtube.com/embed/s-7lyvblFNI" title="YouTube video" allowfullscreen></iframe>
+                   </div>
+
+
+                    <div className="ratio ratio-16x9">
+                          <iframe src="https://www.youtube.com/embed/s6XgAhHNO2k" title="YouTube video" allowfullscreen></iframe>
+                    </div>
+
+                    <div className="ratio ratio-16x9">
+                    <iframe src="https://www.youtube.com/embed/LqXZ628YNj4" title="YouTube video" allowfullscreen></iframe>
+                    </div>
+
+                  <div className="ratio ratio-16x9">
+                  <iframe src="https://www.youtube.com/embed/r7xsYgTeM2Q" title="YouTube video" allowfullscreen></iframe>
+                  </div>
+
+                  
+                  <div className="ratio ratio-16x9">
+                  <iframe src="https://www.youtube.com/embed/jEDCvgAjR5o" title="YouTube video" allowfullscreen></iframe>
+                  </div>
+
+                  <div className="ratio ratio-16x9">
+                  <iframe src="https://www.youtube.com/embed/BIjd73Ynk3M" title="YouTube video" allowfullscreen></iframe>
+                  </div>
+
+                  <div className="ratio ratio-16x9">
+                  <iframe src="https://www.youtube.com/embed/nylNjxeOeSw" title="YouTube video" allowfullscreen></iframe>
+                  </div>
+
+                  <div className="ratio ratio-16x9">
+                  <iframe src="https://www.youtube.com/embed/RI5gOBeTqyE" title="YouTube video" allowfullscreen></iframe>
+                  </div>
+
+
+                </div>
+                    
+            </div>
+        </>
+    )
+}
+
+export default Class
